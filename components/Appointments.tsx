fcd7a01a29b8e66549d3a43be3a1c76ab0745d4e
shamsundarsk <shamsundarjan@gmail.com>
2025-12-08 13:30:40 +0530

    Add actual Cal.com booking link for inline calendar

--- components/Appointments.tsx
+++ components/Appointments.tsx
@@ -1,11 +1,6 @@
 'use client'
 
 export default function Appointments() {
-  // Replace 'YOUR_CAL_USERNAME' with your actual Cal.com username
-  // Replace 'YOUR_EVENT_TYPE' with your event type slug (e.g., '30min', 'consultation')
-  const calUsername = 'YOUR_CAL_USERNAME'
-  const eventType = 'YOUR_EVENT_TYPE'
-  
   return (
     <section id="appointments" className="py-24 px-5 bg-[#111]">
       <div className="max-w-5xl mx-auto text-center">
@@ -19,7 +14,7 @@ export default function Appointments() {
         <div className="bg-black border-2 border-[#d7df23] rounded-xl p-4 md:p-8 overflow-hidden">
           {/* Cal.com iFrame Embed - Shows calendar inline */}
           <iframe
-            src={`https://cal.com/${calUsername}/${eventType}?embed=true&theme=dark`}
+            src="https://cal.com/sham-sham-r9hsdj?embed=true&theme=dark"
             width="100%"
             height="700"
             frameBorder="0"
@@ -29,6 +24,7 @@ export default function Appointments() {
               minHeight: '700px'
             }}
             title="Book a consultation"
+            allow="camera; microphone; autoplay; display-capture"
           />
         </div>
 
